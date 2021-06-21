32fa29668cab7cd338a5b6c7b2cf7b64cea49fcd
Tomáš Hromník <tom.hromnik@gmail.com>
2021-06-21 12:28:44 +0200

    Create CustomCheckbox.vue

---
+++ CustomCheckbox.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-checkbox
+    class="checkbox"
+    v-model="valueLocal"
+    v-bind="$attrs"
+    v-on="$listeners"
+    @change="onChange"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: Boolean,
+  },
+
+  computed: {
+    valueLocal: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+      },
+    },
+  },
+
+  methods: {
+    onChange(val) {
+      this.$emit('input', val);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+// Your custom styles
+.checkbox {
+  display: inline-block;
+  .v-label {
+    font-size: 0.9em;
+  }
+}
+</style>
